b28d3664f4e2e2917b5ff3782a19337f6d50333c
Elvisi2010 <elvisemerllahu12@gmail.com>
2025-04-03 20:42:17 +0200

    Lesson 10
    Day 10

---
+++ Day 10/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Lesson 9</title>
+ </head>
+ <body>
+     <form action="add.php" method="POST">
+         <input type="text" name="name" placeholder="Enter your name"><br>
+         <input type="text" name="surname" placeholder="Enter your surname"><br>
+         <input type="text" name="email" placeholder="Enter your email"><br>
+ 
+         <button type="submit" name="submit">Submit</button>
+     </form>
+
+     <a href="dashboard.php">dashboard</a>
+ </body>
+ </html>
