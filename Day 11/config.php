57f44bd898b53c8798356b44a59ea682466fda0a
Elvisi2010 <elvisemerllahu12@gmail.com>
2025-04-15 20:49:06 +0200

    lesson 11
    day 11

---
+++ Day 11/config.php
@@ -0,0 +1,19 @@
+<?php
+  
+  
+  session_start();
+      $usser="root";
+      $pass="";
+      $server="localhost";
+      $dbname="dbelvis";
+  
+  
+      try{
+          $conn= new PDO("mysql:host=$server; dbname=$dbname",$usser,$pass);
+          echo "connected";
+      }catch(PDOException $e){
+          echo "error : ". $e->getMessage();
+      }
+  
+  
+  ?>
